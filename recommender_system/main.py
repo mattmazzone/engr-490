6adb6b5c8760263841783be23f1b5b525bc2a5d2
Evan Greenstein <evanlg16@gmail.com>
2023-11-08 10:31:47 -0500

    Still thinking about how to use collaborative filtering

--- recommender_system/main.py
+++ recommender_system/main.py
@@ -1,2 +1,10 @@
-from scipy import spatial
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
 
+# Assumptions
+# 1. User is willing to travel the distance
+# 2. User is willing to spend money 
+# 3. Reviews don't matter
+
+
+# Sort recommendations based on, reviews, distance, money spent or activity type
