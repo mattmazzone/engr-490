063751c369c3676bfba6905af74e559b101bd3cb
mattmazzone <M_AZZON@live.concordia.ca>
2023-11-08 16:25:49 -0500

    fixed some pycharm warnings

--- recommender_system/main.py
+++ recommender_system/main.py
@@ -9,15 +9,18 @@ import numpy as np
 
 # User-to-item recommender engine
 
-## Get place vectors
-## Assume these are places that are near the user
+# Get place vectors
+# Assume these are places that are near the user
 places_df = pd.read_csv("places_dataset_sample.csv")
+
 
 def normalize(row: pd.Series) -> pd.Series:
     # don't factor the name of the place into normalization
     if "user_name" in row:
         selected = row.drop(["user_name"])
-    elif "place_name" in row:
+    # elif "place_name" in row:
+    #     selected = row.drop(["place_name"])
+    else:
         selected = row.drop(["place_name"])
 
     # compute the length of the vector using L2 normalization
@@ -28,36 +31,39 @@ def normalize(row: pd.Series) -> pd.Series:
         return row
     return row
 
-## Normalize place vectors
+
+# Normalize place vectors
 places_normalized_df = places_df.apply(normalize, axis=1)
 
-## Get user vectors
-## fetched from our database
+
+# Get user vectors
+# fetched from our database
 historical_user_df = pd.read_csv("historical_user_dataset_sample.csv")
+
 
 user1_df = historical_user_df[historical_user_df["user_name"] == "user 1"]
 user1_places_df = pd.merge(user1_df, places_df, on="place_name")
 
-## Remove the place_name column since its useless now
-user1_summed = user1_places_df.drop(["place_name"], axis=1).groupby("user_name").sum().reset_index();
-## Normalize user vector
+# Remove the place_name column since its useless now
+user1_summed = user1_places_df.drop(["place_name"], axis=1).groupby("user_name").sum().reset_index()
+# Normalize user vector
 user1_sum_normalized = user1_summed.apply(normalize, axis=1)
 
 # Drop useless columns for calculations
 user1_sum_normalized_copy = user1_sum_normalized.drop(["user_name"], axis=1)
 places_normalized_df_copy = places_normalized_df.drop(["place_name"], axis=1)
 
-## Compute similarity matrix for each place
+# Compute similarity matrix for each place
 user1_places_similarity_df = cosine_similarity(user1_sum_normalized_copy, places_normalized_df_copy)
 user1_places_similarity_df = pd.DataFrame(user1_places_similarity_df).transpose()
 user1_places_similarity_df.rename({0: 'similarity'}, axis=1, inplace=True)
 
-#join similarity matrix with places
+# join similarity matrix with places
 user1_places_similarity_df = pd.concat([places_normalized_df, user1_places_similarity_df], axis=1)
-sorted = user1_places_similarity_df.sort_values(by="similarity", ascending=False).reset_index()
+sortedPlaces = user1_places_similarity_df.sort_values(by="similarity", ascending=False).reset_index()
 
-displayed_columns= ["place_name", "similarity" ]
-print(sorted[displayed_columns])
+displayed_columns = ["place_name", "similarity"]
+print(sortedPlaces[displayed_columns])
 
 # TODO: Sort and filter recommendations even further based on, reviews, distance, 
 # money spent or activity type
