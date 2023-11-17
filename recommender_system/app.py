1786faf2d58ea5bdcdf161b3b16c8eede0026540
Evan Greenstein <evanlg16@gmail.com>
2023-11-17 17:17:37 -0500

    Started firebase auth and dynamic routing

--- recommender_system/app.py
+++ recommender_system/app.py
@@ -1,7 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+from firebase_admin import initialize_app, auth, credentials
+import requests
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+cred = credentials.service_account('tripwise-sdk-key.json')
+fb_app = initialize_app(cred)
+
+# Authentication middleware
+def autheticate(id_token):
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        return decoded_token['uid']
+    except Exception as e:
+        print(e)
+
+# Profile route
+@app.route('/profile/<str:userId>', methods=['GET'])
+def profile(userId):
+    try:
+
+        id_token = request.headers.get("Authorization")
+        uid = autheticate(id_token)
+        response = {
+            "status": 200,
+            "msg": "Logged in succesfully",
+            "uid": uid
+        }
+        return make_response(jsonify(response)), 200
+    except Exception as e:
+        print(e)
 
 
-
-app.run(port=5000)
+# Start the server
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
