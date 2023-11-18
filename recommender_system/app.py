aae0688e0b15400b5e3d8b0ad0a996e77cbf5364
Evan Greenstein <evanlg16@gmail.com>
2023-11-17 21:47:11 -0500

    Implemented profile route in flask!!

--- recommender_system/app.py
+++ recommender_system/app.py
@@ -1,37 +1,36 @@
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
-from firebase_admin import initialize_app, auth, credentials
-import requests
+from firebase_admin import initialize_app, auth, credentials, firestore
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-cred = credentials.service_account('tripwise-sdk-key.json')
+cred = credentials.Certificate('tripwise-sdk-key.json')
 fb_app = initialize_app(cred)
+db = firestore.client()
 
 # Authentication middleware
 def autheticate(id_token):
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-        return decoded_token['uid']
-    except Exception as e:
-        print(e)
+    decoded_token = auth.verify_id_token(id_token)
+    return decoded_token['uid']
 
 # Profile route
-@app.route('/profile/<str:userId>', methods=['GET'])
+@app.route('/api/profile/<userId>', methods=['GET'])
 def profile(userId):
     try:
-
         id_token = request.headers.get("Authorization")
-        uid = autheticate(id_token)
-        response = {
-            "status": 200,
-            "msg": "Logged in succesfully",
-            "uid": uid
-        }
-        return make_response(jsonify(response)), 200
+        autheticate(id_token)
+        
+        doc = db.collection("users").document(userId).get()
+        if not doc.exists:
+            return make_response(jsonify({"status": 404, "msg": "User not found" })), 404
+        userData = doc.to_dict()
+        print(userData)
+
+        return make_response(jsonify(userData)), 200
     except Exception as e:
         print(e)
+        return make_response(jsonify({"status": 500, "msg": e.__doc__})), 500
 
 
 # Start the server
