a3329fe5656535dd93aaaf512213c1f0136b59b7
Evan Greenstein <evanlg16@gmail.com>
2024-01-02 17:18:56 -0500

    Communication between backend and recommender flask

--- recommender_system/app.py
+++ recommender_system/app.py
@@ -16,7 +16,6 @@ CORS(app)  # Enable CORS for all routes
 cred = credentials.Certificate('tripwise-sdk-key.json')
 fb_app = initialize_app(cred)
 
-#Mapping user Interests to google API tags
 interests_dict = {
     "Restaurants": ["restaurant", "cafe", "bakery", "bar"],
     "Arts": ["art_gallery", "movie_theater", "museum", "tourist_attraction"],
@@ -34,6 +33,155 @@ interests_dict = {
     "Nature": ["park", "campground", "zoo", "aquarium"],
 }
 
+# https://developers.google.com/maps/documentation/places/web-service/supported_types#table1
+table1_types = [
+  "accounting",
+  "airport",
+  "amusement_park",
+  "aquarium",
+  "art_gallery",
+  "atm",
+  "bakery",
+  "bank",
+  "bar",
+  "beauty_salon",
+  "bicycle_store",
+  "book_store",
+  "bowling_alley",
+  "bus_station",
+  "cafe",
+  "campground",
+  "car_dealer",
+  "car_rental",
+  "car_repair",
+  "car_wash",
+  "casino",
+  "cemetery",
+  "church",
+  "city_hall",
+  "clothing_store",
+  "convenience_store",
+  "courthouse",
+  "dentist",
+  "department_store",
+  "doctor",
+  "drugstore",
+  "electrician",
+  "electronics_store",
+  "embassy",
+  "fire_station",
+  "florist",
+  "funeral_home",
+  "furniture_store",
+  "gas_station",
+  "gym",
+  "hair_care",
+  "hardware_store",
+  "hindu_temple",
+  "home_goods_store",
+  "hospital",
+  "insurance_agency",
+  "jewelry_store",
+  "laundry",
+  "lawyer",
+  "library",
+  "light_rail_station",
+  "liquor_store",
+  "local_government_office",
+  "locksmith",
+  "lodging",
+  "meal_delivery",
+  "meal_takeaway",
+  "mosque",
+  "movie_rental",
+  "movie_theater",
+  "moving_company",
+  "museum",
+  "night_club",
+  "painter",
+  "park",
+  "parking",
+  "pet_store",
+  "pharmacy",
+  "physiotherapist",
+  "plumber",
+  "police",
+  "post_office",
+  "primary_school",
+  "real_estate_agency",
+  "restaurant",
+  "roofing_contractor",
+  "rv_park",
+  "school",
+  "secondary_school",
+  "shoe_store",
+  "shopping_mall",
+  "spa",
+  "stadium",
+  "storage",
+  "store",
+  "subway_station",
+  "supermarket",
+  "synagogue",
+  "taxi_stand",
+  "tourist_attraction",
+  "train_station",
+  "transit_station",
+  "travel_agency",
+  "university",
+  "veterinary_care",
+  "zoo",
+];
+
+# https://developers.google.com/maps/documentation/places/web-service/supported_types#table2
+table2_types = [
+  "administrative_area_level_1",
+  "administrative_area_level_2",
+  "administrative_area_level_3",
+  "administrative_area_level_4",
+  "administrative_area_level_5",
+  "administrative_area_level_6",
+  "administrative_area_level_7",
+  "archipelago",
+  "colloquial_area",
+  "continent",
+  "country",
+  "establishment",
+  "finance",
+  "floor",
+  "food",
+  "general_contractor",
+  "geocode",
+  "health",
+  "intersection",
+  "landmark",
+  "locality",
+  "natural_feature",
+  "neighborhood",
+  "place_of_worship",
+  "plus_code",
+  "point_of_interest",
+  "political",
+  "post_box",
+  "postal_code",
+  "postal_code_prefix",
+  "postal_code_suffix",
+  "postal_town",
+  "premise",
+  "room",
+  "route",
+  "street_address",
+  "street_number",
+  "sublocality",
+  "sublocality_level_1",
+  "sublocality_level_2",
+  "sublocality_level_3",
+  "sublocality_level_4",
+  "sublocality_level_5",
+  "subpremise",
+  "town_square",
+];
+
 # Authentication middleware
 def authenticate(f):
     @wraps(f)
@@ -50,7 +198,7 @@ def authenticate(f):
             return make_response(jsonify({"error": "You are not authorized"}), 401)
     return decorated_function
 
-@app.route('/api/recommend-cold-start', method=['POST'])
+@app.route('/api/recommend-cold-start', methods=['POST'])
 def recommend_cold_start():
     # Get nearby places and user interests array from POST body
     content_type = request.headers.get('Content-Type')
@@ -105,26 +253,27 @@ def normalize(row: pd.Series) -> pd.Series:
 @authenticate
 def recommend():
     request_body = request.json
-    # Get place vectors that correspond to the nearby places
-    places_df = pd.json_normalize(request_body["nearbyPlaceVectors"])
-    # Get user vectors from database
-    historical_user_df = pd.json_normalize(request_body["userVectors"])
-    # join user and places on place_id 
-    user_places_df = pd.merge(historical_user_df, places_df, on="place_name")
-    # Sum all the columns of the newly created user-place table
-    users_summed = user_places_df.drop(["place_name"], axis=1).groupby("user_name").sum().reset_index()
-    # Normalize the user-place table
-    users_sum_normalized = users_summed.apply(normalize, axis=1)
-    # Normalize the places table
-    places_normalized_df = places_df.apply(normalize, axis=1)
-    # Drop useless columns for calculations
-    users_sum_normalized_copy = users_sum_normalized.drop(["user_name"], axis=1)
-    places_normalized_df_copy = places_normalized_df.drop(["place_name"], axis=1)
-    # Get cosine similarity between normalized places table and the normalized user-place table
-    users_places_similarity_df = cosine_similarity(users_sum_normalized_copy, places_normalized_df_copy)
-    # Return the similarity table
-    users_places_similarity_df = pd.DataFrame(users_places_similarity_df).transpose()
-    return pd.DataFrame.to_json(users_places_similarity_df)
+    return request_body
+    # # Get place vectors that correspond to the nearby places
+    # places_df = pd.json_normalize(request_body["nearbyPlaceVectors"])
+    # # Get user vectors from database
+    # historical_user_df = pd.json_normalize(request_body["userVectors"])
+    # # join user and places on place_id 
+    # user_places_df = pd.merge(historical_user_df, places_df, on="place_name")
+    # # Sum all the columns of the newly created user-place table
+    # users_summed = user_places_df.drop(["place_name"], axis=1).groupby("user_name").sum().reset_index()
+    # # Normalize the user-place table
+    # users_sum_normalized = users_summed.apply(normalize, axis=1)
+    # # Normalize the places table
+    # places_normalized_df = places_df.apply(normalize, axis=1)
+    # # Drop useless columns for calculations
+    # users_sum_normalized_copy = users_sum_normalized.drop(["user_name"], axis=1)
+    # places_normalized_df_copy = places_normalized_df.drop(["place_name"], axis=1)
+    # # Get cosine similarity between normalized places table and the normalized user-place table
+    # users_places_similarity_df = cosine_similarity(users_sum_normalized_copy, places_normalized_df_copy)
+    # # Return the similarity table
+    # users_places_similarity_df = pd.DataFrame(users_places_similarity_df).transpose()
+    # return pd.DataFrame.to_json(users_places_similarity_df)
 
 
 # Start the server
