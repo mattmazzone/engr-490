b643aadf797d1e13639052a57ee6c4e4a9b9704f
mattmazzone <M_AZZON@live.concordia.ca>
2024-01-03 15:42:10 -0500

    Made notification modal homescreen for dev

--- frontend/TripWise/App.tsx
+++ frontend/TripWise/App.tsx
@@ -17,6 +17,9 @@ import HomeLogo from "./components/SVGLogos/HomeLogo";
 import TripLogo from "./components/SVGLogos/TripLogo";
 import { PaperProvider } from "react-native-paper";
 
+// import your component here
+import NotificationScreen from "./components/AccountScreen/NotificationScreen";
+
 // Declare your stacks
 const RootStack = createNativeStackNavigator();
 const MainStack = createNativeStackNavigator();
@@ -54,7 +57,8 @@ function BottomTabNavigation() {
         },
       })}
     >
-      <Tab.Screen name="Home" component={Home} />
+      {/*Change back to Home when done*/}
+      <Tab.Screen name="Home" component={NotificationScreen} />
       <Tab.Screen name="Trip" component={Trip} />
       {/* Dont display in nav */}
 
@@ -129,10 +133,8 @@ export default function App() {
   }, []);
 
   return (
-    
-      <NavigationContainer>
-        <RootNavigator user={user} />
-      </NavigationContainer>
-    
+    <NavigationContainer>
+      <RootNavigator user={user} />
+    </NavigationContainer>
   );
 }
