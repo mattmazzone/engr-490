0da5ae3a0f01549ad315f28fad5ac73a70de72db
benhitterman <benhitterman7@gmail.com>
2024-01-10 13:25:45 -0500

    google calendar read only

--- frontend/TripWise/App.tsx
+++ frontend/TripWise/App.tsx
@@ -127,6 +127,7 @@ export default function App() {
     GoogleSignin.configure({
       iosClientId:
         "425734765321-poj5etgv9nffmi4n42mj6b1sroajrq4c.apps.googleusercontent.com",
+        scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
     });
     const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user: any) => {
       setUser(user);
