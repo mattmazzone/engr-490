1a1822bc7822b36f19b0faf99ed0220d494f022e
mattmazzone <M_AZZON@live.concordia.ca>
2024-01-13 00:42:14 -0500

    cleaned up account screen

--- frontend/TripWise/hooks/useUserProfile.ts
+++ frontend/TripWise/hooks/useUserProfile.ts
@@ -1,17 +1,22 @@
-// useUserProfile.js
 import { useState, useEffect } from "react";
 import * as UserService from "../services/userServices";
 import { UserProfile } from "../types/userTypes";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const useUserProfile = () => {
+interface UserProfileHook {
+  refreshData: boolean;
+}
+
+export const useUserProfile = ({ refreshData }: UserProfileHook) => {
   const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
   const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
-    const fetchProfile = async () => {
+    const fetchProfileFromDB = async () => {
       try {
         const profile = await UserService.fetchUserProfile();
         setUserProfile(profile);
+        await AsyncStorage.setItem("userProfile", JSON.stringify(profile));
       } catch (error) {
         console.error("Error fetching user profile:", error);
       } finally {
@@ -19,8 +24,29 @@ export const useUserProfile = () => {
       }
     };
 
-    fetchProfile();
-  }, []);
+    const fetchProfileFromStorage = async () => {
+      try {
+        const storedProfile = await AsyncStorage.getItem("userProfile");
+        if (storedProfile !== null) {
+          setUserProfile(JSON.parse(storedProfile));
+        } else {
+          // No profile found in storage, fetch from DB
+          await fetchProfileFromDB();
+        }
+      } catch (error) {
+        console.error("Error fetching user profile from storage:", error);
+        await fetchProfileFromDB(); // Fallback to fetching from DB in case of error
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    if (refreshData) {
+      fetchProfileFromDB();
+    } else {
+      fetchProfileFromStorage();
+    }
+  }, [refreshData]);
 
   return { userProfile, isFetching };
 };
