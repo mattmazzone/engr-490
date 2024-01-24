5354dd5db6796e96945246f301bde57af611cd56
Evan Greenstein <evanlg16@gmail.com>
2024-01-24 16:50:08 -0500

    Just copied utils.py

--- recommender_system/sample/v2/recommender.py
+++ recommender_system/sample/v2/recommender.py
@@ -3,24 +3,26 @@ import pandas as pd
 import numpy as np
 import os
 import json
-from recommender_system.src.utils import create_df, create_rating_df
+import platform
+from utils import create_df, create_rating_df
 
-script_path= os.path.dirname(__file__) + "/"
+# Step 1 - Get nearby places
+script_path= os.path.dirname(__file__)
 
-npf = open(script_path + 'nearbyPlaces.json')
+npf = open(os.path.join(script_path, 'nearbyPlaces.json'))
 nearby_places = json.load(npf)
 
-rpf = open(script_path + 'recentPlace.json')
+rpf = open(os.path.join(script_path, 'recentPlaces.json'))
 recent_places = json.load(rpf)
+
+# Step 2 - Turn them into dfs not including user rating
 
 nearby_places_df = create_df(nearby_places)
 recent_places_df = create_df(recent_places)
 
-recent_place_ratings_df = create_rating_df(recent_places)
-print(nearby_places_df)
-print(recent_places_df)
+# Step 3 - Create user rating df for the recent places
 
-print(recent_place_ratings_df)
+recent_place_ratings_df = create_rating_df(recent_places)
 
 
 npf.close()
