c8348127049c5779a54abe94f455cf672e7cac2d
mattmazzone <M_AZZON@live.concordia.ca>
2024-01-29 13:30:12 -0500

    removed animations

--- frontend/TripWise/components/HomeScreen/PastTrips.tsx
+++ frontend/TripWise/components/HomeScreen/PastTrips.tsx
@@ -1,45 +1,20 @@
 import React, { useContext, useState } from "react";
-import {
-  View,
-  StyleSheet,
-  ScrollView,
-  Animated,
-  NativeSyntheticEvent,
-  NativeScrollEvent,
-} from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ThemeContext from "../../context/ThemeContext";
 import PastTrip from "./PastTrip";
 
-const PastTrips = ({ onScroll, isFetching, pastTrips, scrollY }: any) => {
+const PastTrips = ({ isFetching, pastTrips }: any) => {
   const { theme } = useContext(ThemeContext);
-
-  const headerHeight = 135; // Adjust based on your header's height
-  const translationThreshold = 50; // The scroll offset at which translation stops and normal scroll begins
-
-  // Translate ScrollView upwards
-  const translate = scrollY.interpolate({
-    inputRange: [0, 50, 50 + 1],
-    outputRange: [185, 50, 50],
-    extrapolate: "clamp",
-  });
-
-  const animatedStyle = {
-    transform: [{ translateY: translate }],
-  };
 
   return (
     <View style={styles.container}>
-      <Animated.ScrollView
-        scrollEventThrottle={16} // Adjust as needed for performance
-        onScroll={onScroll}
-        style={[styles.scrollView, animatedStyle]}
-      >
+      <ScrollView style={[styles.scrollView]}>
         <View style={[styles.content]}>
           {pastTrips.map((trip: any, index: any) => (
             <PastTrip key={index} pastTrip={trip} />
           ))}
         </View>
-      </Animated.ScrollView>
+      </ScrollView>
     </View>
   );
 };
