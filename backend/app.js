9ee77d73fd4d15fb2a15b1cdf88fbe66498750e6
root <root@vmi1627789.contaboserver.net>
2024-01-31 15:22:18 -0600

    adding config files for pm2

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,6 @@ app.use("/api", recommendedActivitiesRoutes);
 
 // Start the server
 const port = 3000;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
