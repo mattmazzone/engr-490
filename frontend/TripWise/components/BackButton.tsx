e71b2ed7953c1d4da5956f18b5762e73b490917b
benhitterman <benhitterman7@gmail.com>
2024-02-02 18:12:01 -0500

    major changes to login and sign up pages

--- frontend/TripWise/components/BackButton.tsx
+++ frontend/TripWise/components/BackButton.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import BackButtonIcon from './SVGLogos/BackButtonIcon';
 
 const BackButton = () => {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity onPress={() => navigation.goBack()}>
-      <Text>Back</Text>
+      <BackButtonIcon focused/>
     </TouchableOpacity>
   );
 };
