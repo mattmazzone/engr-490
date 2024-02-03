d77b619fd18b186a2ad5e2c323c3dac943464637
Alex <santellialex@gmail.com>
2024-02-02 19:10:35 -0500

    changing API_URL based off Node Environment

--- frontend/TripWise/services/userServices.ts
+++ frontend/TripWise/services/userServices.ts
@@ -9,10 +9,16 @@ import { Meeting, TripType } from "../types/tripTypes";
 
 // Base API URL
 let BASE_API_URL: string;
-if (Platform.OS === "android") {
-  BASE_API_URL = "http://10.0.2.2:3000/api";
-} else {
-  BASE_API_URL = "https://api.tripwise.cloud/api";
+
+if (process.env.NODE_ENV = 'production') {
+  BASE_API_URL = "https://api.tripwise.cloud/api"
+}
+else {
+  if (Platform.OS === "android") {
+    BASE_API_URL = "http://10.0.2.2:3000/api";
+  } else {
+    BASE_API_URL = "http://localhost:3000/api";
+  }
 }
 
 // Function to create a new user
