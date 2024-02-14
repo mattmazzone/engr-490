ef0d8def0d5bec8b8c52c9973b69e39e372ca1e3
Alex <santellialex@gmail.com>
2024-02-14 17:16:52 -0500

    added auto search for location when adding Meeting

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -304,4 +304,20 @@ router.get("/past_trips/:uid/:tripId", authenticate, async (req, res) => {
   }
 });
 
+router.get("/searchAddress:query", async (req, res) => {
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+  const userQuery = req.params.query;
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}&input=${encodeURIComponent(userQuery)}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching from Google Places API:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+
+});
+
 module.exports = router;
