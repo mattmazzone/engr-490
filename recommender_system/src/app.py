a964e1127de33c4db4ec6105c73c01d3254f380b
Evan Greenstein <evanlg16@gmail.com>
2024-02-15 23:58:03 -0500

    Update app.py

--- recommender_system/src/app.py
+++ recommender_system/src/app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
 cred = credentials.Certificate(os.path.join(
-    os.path.dirname(__file__), 'tripwise-sdk-key.json'))
+    os.path.dirname(__file__), '../tripwise-sdk-key.json'))
 fb_app = initialize_app(cred)
 
 interests_dict = {
