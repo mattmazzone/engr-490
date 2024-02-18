bfaff1b242250fd5074360f72194dd41d61102af
Alex <santellialex@gmail.com>
2024-02-18 17:38:54 -0500

    uses one random interest per meeting to get restaurants

--- backend/utils/here.js
+++ backend/utils/here.js
@@ -1,37 +1,50 @@
+const axios = require("axios");
+
 
 
 async function getRestaurants(latitude, longitude, limit, userInterests) {
     // Filter out all the non-restaurant user interests
     const filteredRestaurantInterests = userInterests.filter(interest => interest.includes("restaurant"));
+    
+    //Remove unnecessary "_restaurant" in interests
+    const cleanedInterests = filteredRestaurantInterests.map(interest => interest.replace("_restaurant", ""));
+    //Randomize array and select 1 random food related interests
+
+
   
-    // Join the filtered interests with spaces to form the query
-    const result = filteredRestaurantInterests.join(" ");
+    const result = selectRandomItem(cleanedInterests);
   
     // Define the API endpoint and parameters
     const url = "https://discover.search.hereapi.com/v1/discover";
     const params = new URLSearchParams({
       at: `${latitude},${longitude}`,
-      q: result,
+      q: `${result} restaurant`,
       limit: limit, //Can set limit
       apiKey: process.env.HERE_API_KEY 
     });
   
     try {
       // Make the GET request
-      const response = await fetch(`${url}?${params.toString()}`);
-      if (response.ok) { // Check if the response status is 200-299
-        const data = await response.json();
-
+      const response = await axios.get(url, { params });
+      if (response.status >= 200 && response.status < 300) { // Check if the response status is 200-299
+        const data = response.data;
+    
         const processedData = processRestaurantData(data);
         return processedData; // Return the formatted data
       } else {
         console.error("Failed to fetch restaurants:", response.status);
-        return null; 
+        return null;
       }
     } catch (error) {
       console.error("Error fetching restaurants:", error);
-      return null; // Handle fetch errors
+      return null; // Handle axios errors
     }
+}
+
+
+function selectRandomItem(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
 }
 
 async function processRestaurantData(data){
