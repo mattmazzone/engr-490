9d037b04d446b7ab80ea926b16c2717ca42948c2
Evan Greenstein <evanlg16@gmail.com>
2024-04-03 15:05:37 -0400

    Started evaluation

---
+++ recommender_system/evaluation/data_sources.py
@@ -0,0 +1,538 @@
+
+#  Old montreal { "latitude": 45.512405, "longitude":  -73.548584}
+#  GTA { "latitude": 44.0384, "longitude":  -79.2000}
+
+places_db = [
+    {
+        "id": "ChIJI5sa9wkbyUwRwzI4DcnZP84",
+        "types": [
+            "tourist_attraction",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "La Grande Roue de Montréal",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJKSezr_gayUwRDnKQVi-nC4U",
+        "types": [
+            "tourist_attraction",
+            "historical_landmark",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Vieux-Port de Montréal",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJH1lQy1UayUwRaUKBtKz7mOg",
+        "types": [
+            "shopping_mall",
+            "tourist_attraction",
+            "historical_landmark",
+            "event_venue",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Bonsecours Market",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJcdW2wVQayUwRh0405vZfjNo",
+        "types": [
+            "hotel",
+            "lodging",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Hyatt Centric Montreal",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJfTEP_v8ayUwRwXdj0_r-O0w",
+        "types": [
+            "historical_landmark",
+            "tourist_attraction",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Clock Tower",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJJYd8aVYbyUwRWEOeInIxA9g",
+        "types": [
+            "bakery",
+            "cafe",
+            "bar",
+            "store",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "La Cave à Manger BOULANGERIE Vieux-Montréal",
+            "languageCode": "fr"
+        }
+    },
+    {
+        "id": "ChIJi5muTFQayUwRNUBN7m-Nuo8",
+        "types": [
+            "museum",
+            "tourist_attraction",
+            "church",
+            "place_of_worship",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Notre-Dame-de-Bon-Secours Chapel",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJgUvIbFUayUwRuThZ1owWITA",
+        "types": [
+            "parking",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Quai de l'Horloge",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJsbppnhIZyUwRNltZ80Tw0nQ",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Lightspeed Commerce Montreal",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJJaoitVUayUwR-HuAiVhF5qU",
+        "types": [
+            "bar",
+            "restaurant",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "BreWskey Pub",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJs6eYQ1QayUwRmCalaznbXlM",
+        "types": [
+            "restaurant",
+            "coffee_shop",
+            "cafe",
+            "store",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Muru Crêpe",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJifcXRf8ayUwRMaJwx681w-w",
+        "types": [
+            "event_venue",
+            "sports_complex",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "La Patinoire du Vieux-Port de Montréal",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJl8kWQsMayUwRgbAKuCPZ5PE",
+        "types": [
+            "tourist_attraction",
+            "amusement_park",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Voiles en Voiles",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJ29D638MbyUwRXsp4eG07Pc4",
+        "types": [
+            "restaurant",
+            "bar",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "BreWskey Taproom",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJVyru34obyUwRrp16axQ4YN4",
+        "types": [
+            "hotel",
+            "lodging",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Hygie Hotel",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJVcoRvVUayUwRYPNpd36fmZw",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Parking Car",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJQzuin08ayUwRomJsPQCKF64",
+        "types": [
+            "coffee_shop",
+            "florist",
+            "cafe",
+            "store",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "L'Orbite",
+            "languageCode": "fr"
+        }
+    },
+    {
+        "id": "ChIJAUCQ8a8byUwRPbs0Rks0E6s",
+        "types": [
+            "tourist_attraction",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "SOS Labyrinthe Vieux-Port de Montréal",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJa1TsNFQayUwRsyZ2zcTixkQ",
+        "types": [
+            "restaurant",
+            "bar",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Maison Saint-Paul",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJw3Hu7lQayUwR41vDXymmnss",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Cirque Éloize",
+            "languageCode": "fr"
+        }
+    },
+
+    {
+        "id": "ChIJW_iM5esv1YkRqyvJdodclyA",
+        "types": [
+            "restaurant",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Annina's Bakeshop & Catering",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJJx_DntYl1YkRjVv2HH_JIA8",
+        "types": [
+            "tourist_attraction",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Schitt’s Creek Film Location",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJ1z_P8Ko61YkROx1O21IIG0c",
+        "types": [
+            "school",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Goodwood Public School",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJsbLUCFUl1YkRj5H4kE9Ng-4",
+        "types": [
+            "gas_station",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Esso",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJ_wqLGbMt1YkR29sHbrtxJN4",
+        "types": [
+            "community_center",
+            "event_venue",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Goodwood Community Centre",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJO3VkaCIl1YkR0g4a0lSpx2M",
+        "types": [
+            "sandwich_shop",
+            "fast_food_restaurant",
+            "meal_takeaway",
+            "restaurant",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Subway",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJCeT_2_Qs1YkRwugExnq4kng",
+        "types": [
+            "child_care_agency",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "All About Kids Child Care Centre",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJJbJ4DFUl1YkRP4sOOGa0zqQ",
+        "types": [
+            "convenience_store",
+            "store",
+            "food",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "One Stop Convenience & LCBO",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJFcLkFtEl1YkRiYHf16gLQ1I",
+        "types": [
+            "tourist_attraction",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "“Bob’s Garage” from Schitt’s Creek",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJf15NtlQl1YkRj9BmGTlHxg4",
+        "types": [
+            "bus_stop",
+            "transit_station",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Hwy. 47 @ Front St.",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJq9i1OLEl1YkRY-2aa5aag40",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Blackberry Clinic Goodwood",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJV61hj1Ul1YkRMuruFJU0FcY",
+        "types": [
+            "church",
+            "place_of_worship",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Goodwood Baptist Church",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJ6Y0FY1Ul1YkRfY3cCSxJeKI",
+        "types": [
+            "electronics_store",
+            "home_goods_store",
+            "store",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Bart The Appliance Man",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJpcu9kfUl1YkRpyM6pw3angg",
+        "types": [
+            "clothing_store",
+            "store",
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Wanting Style and Spa",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJv8kFaMEXK4gR_hPq2XfBP2A",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Asopa Films Inc.",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJE4lclHsv1YkR84OePZ_rm80",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Disposeofit.ca",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJF7ghxFQl1YkRhKl5T_5dp9s",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "INDUNA ART & DESIGN INC.",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJq6omf_8v1YkR_nnh2NalhyY",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Mycosource",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJx5ByC9Mu1YkR-kuF4N2TUwc",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "My Massage Therapist",
+            "languageCode": "en"
+        }
+    },
+    {
+        "id": "ChIJIf6UDBsv1YkR3-DWGOtPwBk",
+        "types": [
+            "point_of_interest",
+            "establishment"
+        ],
+        "displayName": {
+            "text": "Ultra Safe Canada",
+            "languageCode": "en"
+        }
+    }
+
+
+]
+
+# Ordering of historical data:
+#   0th element = earliest
+#   last element = most recent
+
+historical1 = [
+
+]
+
+historical2 = [
+
+]
+
+historical3 = [
+
+]
---
+++ recommender_system/evaluation/recommender_systems.py
@@ -0,0 +1,18 @@
+
+from sklearn.metrics.pairwise import cosine_similarity
+from pandas import DataFrame
+import pandas as pd
+import numpy as np
+
+
+# Recommendation system using collaborative filtering item-item
+# 
+# Returns top k places with similarity scores
+def full_recommender(k: int = 10) -> DataFrame:
+    pass
+
+# Recommendation system using only interests of user
+# 
+# Returns top k places with similarity scores
+def simple_interests_recommender(k: int = 10) -> DataFrame:
+    pass
